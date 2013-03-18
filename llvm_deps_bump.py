1a5f2698a97dcf3fd366018c6378c29cbff5598d
Derek Schuff <dschuff@chromium.org>
2013-03-18 14:55:21 -0700

    Bring back uility functions which previously lived in the deleted nacl_deps_bump script

--- llvm_deps_bump.py
+++ llvm_deps_bump.py
@@ -5,15 +5,42 @@
 
 import optparse
 import os
+import re
 import subprocess
 import sys
 
-from nacl_deps_bump import ReadFile, WriteFile, GetDepsField, SetDepsField
-import nacl_deps_bump
-
-
 LLVM_DIR = 'pnacl/git/llvm'
 
+def ReadFile(filename):
+  fh = open(filename, "r")
+  try:
+    return fh.read()
+  finally:
+    fh.close()
+
+
+def WriteFile(filename, data):
+  fh = open(filename, "w")
+  try:
+    fh.write(data)
+  finally:
+    fh.close()
+
+def MatchKey(data, key):
+  match = re.search('^\s*"%s":\s*"(\S+)",\s*(#.*)?$' % key, data, re.M)
+  if match is None:
+    raise Exception('Key %r not found' % key)
+  return match
+
+def GetDepsField(data, key):
+  match = MatchKey(data, key)
+  return match.group(1)
+
+def SetDepsField(data, key, value):
+  match = MatchKey(data, key)
+  return ''.join([data[:match.start(1)],
+                  value,
+                  data[match.end(1):]])
 
 def GetCommandOutput(command, **kwargs):
   proc = subprocess.Popen(command, stdout=subprocess.PIPE, **kwargs)
